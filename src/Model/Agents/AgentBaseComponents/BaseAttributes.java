de4672ee3795bba085ebab3be154278bfcc9a597
sam <samburchmore@gmail.com>
2023-01-23 21:04:59 +0000

    reworked base scores and attributes

--- src/Model/Agents/AgentBaseComponents/BaseAttributes.java
+++ src/Model/Agents/AgentBaseComponents/BaseAttributes.java
@@ -1,9 +1,8 @@
 package Model.Agents.AgentBaseComponents;
 
 import Model.Agents.AgentInterfaces.Attributes;
-import Model.Agents.AgentStructs.AgentType;
 
-import java.util.Random;
+import java.awt.*;
 
 public abstract class BaseAttributes implements Attributes {
 
@@ -11,124 +10,84 @@ public abstract class BaseAttributes implements Attributes {
     private int size;
     private int vision;
     private int eatAmount;
-    private AgentType agentType;
-    Random random = new Random();
+    private int creationDelay;
+    private int maxEnergy;
+    private int maxAge;
+    private int agentCode;
+    private Color agentColour;
 
-    public BaseAttributes(int speed_, int size_, int vision_, int eatAmount_, AgentType agentType_) {
-        this.speed = speed_;
-        this.size = size_;
-        this.vision = vision_;
-        this.eatAmount = eatAmount_;
-        this.agentType = agentType_;
+    public BaseAttributes(int speed, int size, int vision, int eatAmount, int creationDelay, int maxEnergy, int maxAge, int agentCode, Color agentColour) {
+        this.speed = speed;
+        this.size = size;
+        this.vision = vision;
+        this.eatAmount = eatAmount;
+        this.creationDelay = creationDelay;
+        this.maxEnergy = maxEnergy;
+        this.maxAge = maxAge;
+        this.agentCode = agentCode;
+        this.agentColour = agentColour;
     }
 
-    public BaseAttributes(Attributes attributes_a, Attributes attributes_b){
-        // set the new agents type.
-        this.agentType = attributes_a.getType();
-        this.speed = attributes_a.getSpeed();
-        this.size = attributes_a.getSize();
-        this.vision = attributes_a.getVision();
-        this.eatAmount = attributes_a.getEatAmount();
+    public BaseAttributes(Attributes attributesA, Attributes attributesB){
+        this.speed = attributesA.getSpeed();
+        this.size = attributesA.getSize();
+        this.vision = attributesA.getVision();
+        this.eatAmount = attributesA.getEatAmount();
+        this.creationDelay = attributesA.getCreationDelay();
+        this.maxEnergy = attributesA.getMaxEnergy();
+        this.maxAge = attributesA.getMaxAge();
+        this.agentCode = attributesA.getAgentCode();
+        this.agentColour = attributesA.getAgentColour();
     }
 
-    public void mutate() {
-        if (this.random.nextInt(100) > 98) {
-            // We've decided the agent is going to mutate, now we need to randomly decide what attribute to mutate.
-            switch (this.random.nextInt(3)) {
-                case 0: // Mutate speed
-                    this.speed += this.mutationMagnitude();
-                    break;
-                case 1: // Mutate size
-                    this.size += this.mutationMagnitude();
-                    break;
-                case 2: // Mutate vision
-                    this.vision += this.mutationMagnitude();
-                    break;
-            }
-        }
+    @Override
+    public String toString() {
+        return "Movement Range: " + speed + ". Size: " + size + ". Vision Range: " + vision + ". Eat Amount: " + eatAmount + ". Creation Delay: " + creationDelay + ". Max Energy: " + maxEnergy + ". Max Age: " + maxAge + ".";
     }
 
-    /**
-     * @return
-     * This method returns an integer value between -5 and +5 excluding 0.
-     * A magnitude is first calculated, a lower magnitude has a higher probability
-     * of being returned. Before being returned, the magnitude is combined with a
-     * modifier which has an equal chance of being -1 or +1.
-     */
-    public int mutationMagnitude() {
-        // Used to make the weighted decision
-        int r = this.random.nextInt(100);
-        int modifier = this.random.nextInt(2);
-        if (modifier == 0) {
-            modifier -= 1;
-        }
-        if (r < 45) {
-            return modifier;
-        }
-        else if (r < 70) {
-            return 2*modifier;
-        }
-        else if (r < 85) {
-            return 3*modifier;
-        }
-        else if (r < 95) {
-            return 4*modifier;
-        }
-        else {
-            return 5*modifier;
-        }
-    }
-
+    @Override
     public int getSpeed() {
-        return this.speed;
+        return speed;
     }
 
+    @Override
     public int getSize() {
-        return this.size;
+        return size;
     }
 
+    @Override
     public int getVision() {
-        return this.vision;
+        return vision;
     }
 
     @Override
     public int getEatAmount() {
-        return this.eatAmount;
+        return eatAmount;
     }
 
-    public AgentType getType() {
-        return this.agentType;
+    @Override
+    public int getCreationDelay() {
+        return creationDelay;
     }
 
-    public void setSpeed(int speed) {
-        this.speed = speed;
+    @Override
+    public int getMaxEnergy() {
+        return maxEnergy;
     }
 
-    public void setSize(int size) {
-        this.size = size;
+    @Override
+    public int getMaxAge() {
+        return maxAge;
     }
 
-    public void setVision(int vision) {
-        this.vision = vision;
+    @Override
+    public int getAgentCode() {
+        return agentCode;
     }
 
-    public void setAgentType(AgentType agentType) {
-        this.agentType = agentType;
-    }
-
-    public Random getRandom() {
-        return this.random;
-    }
-
-    public void setRandom(Random random) {
-        this.random = random;
-    }
-
-    public void setEatAmount(int eatAmount) {
-        this.eatAmount = eatAmount;
-    }
-
-    public AgentType getAgentType() {
-        return this.agentType;
+    @Override
+    public Color getAgentColour() {
+        return agentColour;
     }
 }
+
--- src/Model/Agents/AgentBaseComponents/BaseScores.java
+++ src/Model/Agents/AgentBaseComponents/BaseScores.java
@@ -5,114 +5,66 @@ import Model.Agents.AgentInterfaces.Scores;
 public abstract class BaseScores implements Scores {
 
     private int hunger;
-    private int health;
     private int age;
-    private int MAX_HUNGER;
-    private int MAX_HEALTH;
-    private int MAX_AGE;
-    private int creationDelay;
+    private int maxHunger;
+    private int maxAge;
     private int creationCounter;
 
-    public BaseScores(int hunger_, int health_, int age_, int MAX_HUNGER_, int MAX_HEALTH_, int MAX_AGE_, int creationDelay_) {
-        this.hunger = hunger_;
-        this.health = health_;
-        this.age = age_;
-        this.MAX_HUNGER = MAX_HUNGER_;
-        this.MAX_HEALTH = MAX_HEALTH_;
-        this.MAX_AGE = MAX_AGE_;
-        this.creationDelay = creationDelay_;
-        this.creationCounter = 0;
-
+    public BaseScores(int hunger, int age, int maxHunger, int maxAge, int creationCounter) {
+        this.hunger = hunger;
+        this.age = age;
+        this.maxHunger = maxHunger;
+        this.maxAge = maxAge;
+        this.creationCounter = creationCounter;
     }
 
     @Override
-    public void setHunger(int hunger_) {
-        this.hunger = hunger_;
-        if (this.hunger > this.MAX_HUNGER) {
-            this.hunger = this.MAX_HUNGER;
-        }
-        //System.out.println(this.hunger);
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
     }
 
     @Override
     public int getHunger() {
-        return this.hunger;
+        return hunger;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
     }
 
     @Override
     public int getAge() {
-        return this.age;
+        return age;
     }
 
     @Override
-    public void setAge(int age_) {
-        this.age = age_;
+    public void setMaxEnergy(int maxHunger) {
+        this.maxHunger = maxHunger;
     }
 
     @Override
-    public int getHealth() {
-        return this.health;
+    public int getMaxEnergy() {
+        return maxHunger;
     }
 
     @Override
-    public void setHealth(int health_) {
-        this.health = health_;
-        if (this.health > this.MAX_HEALTH) {
-            this.health = this.MAX_HEALTH;
-        }
+    public void setMaxAge(int maxAge) {
+        this.maxAge = maxAge;
     }
 
     @Override
-    public int getMAX_HUNGER() {
-        return this.MAX_HUNGER;
+    public int getMaxAge() {
+        return maxAge;
     }
 
     @Override
-    public int getMAX_AGE() {
-        return this.MAX_AGE;
-    }
-
-    @Override
-    public int getMAX_HEALTH() {
-        return this.MAX_HEALTH;
-    }
-
-    @Override
-    public void setMAX_HUNGER(int MAX_HUNGER_) {
-        this.MAX_HUNGER = MAX_HUNGER_;
-    }
-
-    @Override
-    public void setMAX_AGE(int MAX_AGE_) {
-        this.MAX_AGE = MAX_AGE_;
-    }
-
-    @Override
-    public void setMAX_HEALTH(int MAX_HEALTH_) {
-        this.MAX_HEALTH = MAX_HEALTH_;
-    }
-
-    @Override
-    public int getCreationDelay() {
-        return this.creationDelay;
-    }
-
-    @Override
-    public void setCreationDelay(int creationDelay_) {
-        this.creationDelay = creationDelay_;
-        if (this.creationDelay <= 0) {
-            this.creationDelay = 0;
-        }
-    }
-
-    public int getCreationCounter() {
-        return this.creationCounter;
-    }
-
     public void setCreationCounter(int creationCounter) {
         this.creationCounter = creationCounter;
-        if (this.creationCounter < 0) {
-            this.creationCounter = 0;
-        }
+    }
+
+    @Override
+    public int getCreationCounter() {
+        return creationCounter;
     }
 }
